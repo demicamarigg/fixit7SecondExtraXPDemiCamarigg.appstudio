9e513b939dd4064a94cc16e6b6fe769a7fb3e04b
demicamarigg <71406599+demicamarigg@users.noreply.github.com>
2020-11-25 09:23:24 -0600

    Initial commit

---
+++ forms/Form1/Form1.js
@@ -0,0 +1,11 @@
+let rows = parseInt(prompt("Enter the number of rows you want shown."))
+let columns = parseInt(prompt("Enter the number of columns you want shown."))
+
+for (i = 1; i<=rows; i++){
+for (j = 1;j<=columns;j++){
+  
+document.write("*")
+}
+
+document.write("<br />")
+}
